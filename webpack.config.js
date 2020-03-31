838a7faa3e723b4bbd4bdd5cad1c72cacf34d31d
icobra <eiroglif@mail.ru>
2020-03-31 09:07:06 +0300

    Pug plugin don't work

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,35 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    module: {
+        module:{
+            rules: [
+    
+                {
+                    test: /\.pug$/,
+                    use: [
+                        {
+                            loader: 'html-loader',
+                        },
+                        {
+                            loader: 'pug-html-loader',
+                        },
+                    ],
+                },
+    
+            ],
+        },
+    plugins: [
+        new HtmlWebpackPlugin({
+          // Required
+          filename: 'index.html',
+          template: './src/hello.pug',
+        })
+    ]
 }
