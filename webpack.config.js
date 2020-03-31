765944c2d0d16df2f3e833f82ebb4198078cadfd
icobra <eiroglif@mail.ru>
2020-03-31 10:54:20 +0300

    New instruction

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
-const path = require('path')
+// Конфигурационный файл в стиле https://webpack.js.org/configuration/ .
+// Позволяет node.js обрабатывать пути на разных платформах одинаково.
+const path = require('path');
+// Для работы с html файлами и шаблонами, также понадобится для работы с pug.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const PATHS = {
+  source: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'dist')
+};
+
+// Экспорт модуля в node.js
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  // Точка входа в приложение.
+  entry: PATHS.source + 'app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.build
+  },
+
+  // plugins - плагины для кастомизации процесса сборки Webpack.
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack app',
+      template: './src/index.pug'
+    }),
+  ]
+
+  // ??? .
+  module: {
+    rules: [
+    {
+    test: /\.pug$/,
+    loader: 'pug-loader',
+    options: {
+      pretty: true
+      },
+    }]
+  }
 }
